056d420908a8e01b9325e4bf153ff3ba3e9234d6
Leonid Drobotov <fromarshincevo95@gmail.com>
2023-07-20 21:26:38 +0300

    Added a solution for task_25

---
+++ task_25/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+//
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Exponentiation(int number_input, int number_pow_input)
+{
+    int Pow = 1;
+    for (int i = 1; i <= number_pow_input; i++)
+    {
+        Pow = Pow * number_input;
+    }
+
+    return Pow;
+}
+
+Console.Write("Введите число: ");
+int number_input = int.Parse(Console.ReadLine());
+Console.Write("Введите степень в которе возведем число: ");
+int number_pow_input = int.Parse(Console.ReadLine());
+
+int exponentiation = Exponentiation(number_input, number_pow_input);
+Console.WriteLine($"{number_input} -> {exponentiation}");
